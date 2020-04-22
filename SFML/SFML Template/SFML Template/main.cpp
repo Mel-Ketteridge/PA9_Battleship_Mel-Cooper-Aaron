2907b21cf76dc067455d346b93e68670965453e6
Mel-Ketteridge <63305600+Mel-Ketteridge@users.noreply.github.com>
2020-04-21 22:10:32 -0700

    Sup

--- SFML/SFML Template/SFML Template/main.cpp
+++ SFML/SFML Template/SFML Template/main.cpp
@@ -1,19 +1,37 @@
 #include <SFML\Graphics.hpp>
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(640,480), "SFML Tutorial");
-	window.display();
+	sf::RenderWindow window(sf::VideoMode(1000,1000), "First Try");
+	sf::Vector2f shape(110, 110);
+	sf::RectangleShape rect(shape);
 
-	/*while (window.isOpen()) {
+
+	while (window.isOpen()) {
 		//Handle events
+
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
 
 		//update
 		window.clear();
 
 		//Draw objects
 
+		for (int i = 0; i < 50; i++) {
+			for (int j = 0; j < 50; j++) {
+				rect.setPosition((110 * i) + 15* i, (110 * j) + 15 * j);
+				window.draw(rect);
+			}
+		}
+
 		window.display();
-	}*/
+	}
 
 	return 0;
 }
